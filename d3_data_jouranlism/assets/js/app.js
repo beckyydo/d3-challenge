139d8cf8d6a16bfb294f8c7736cf89b427d29efa
beckyydo <47190773+beckyydo@users.noreply.github.com>
2021-01-23 20:08:47 -0500

    update axis

--- d3_data_jouranlism/assets/js/app.js
+++ d3_data_jouranlism/assets/js/app.js
@@ -1,18 +1,21 @@
 // @TODO: YOUR CODE HERE!
-var svgHeight = window.innerHeight;
-var svgWidth = window.innerWidth;
+// Set SVG area
+var svgHeight = 500;
+var svgWidth = 600;
 
+// Set margin
 var margin = {
-  top: 20,
-  right: 40,
-  bottom: 60,
-  left: 50
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
 };
 
+// Set chart area
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
-var svg = d3.select("body")
+var svg = d3.select("#scatter")
             .append("svg")
             .attr("width", svgWidth)
             .attr("height", svgHeight);
@@ -20,49 +23,66 @@ var svg = d3.select("body")
 var chartGroup = svg.append("g")
                     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-
+// Load Data (Poverty vs Healthcare)                    
 d3.csv("assets/data/data.csv").then(function(data){
 
     data.forEach(function(entry) {
         entry.healthcare = +entry.healthcare;
         entry.poverty = +entry.poverty;
+        console.log(entry.healthcare);
       });
 
-    console.log(data);
-
     // Set Axis Ranges
-    var xScale = d3.scaleTime()
-                .domain([0, d3.max(data, d => d.poverty)])
+    var xScale = d3.scaleLinear()
+                .domain([d3.min(data, d => d.poverty)-5 , d3.max(data, d => d.poverty)])
                 .range([0, width]);
 
     var yScale = d3.scaleLinear()
-                .domain([0, d3.max(data, d=> d.healthcare)])
+                .domain([-2, d3.max(data, d => d.healthcare)])
                 .range([svgHeight, 0]);
 
     var bottomAxis = d3.axisBottom(xScale);
     var leftAxis = d3.axisLeft(yScale);
 
-
+    // Append X-Axis
     chartGroup.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis)
-
     chartGroup.append("g")
         .call(leftAxis);  
 
-    var line = d3.line()
-                .x(d => xScale(d.poverty))
-                .y(d => yScale(d.healthcare));
-
+    // Create circle/scatterplots
     var circlesGroup = chartGroup.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
         .attr("cx", d => xScale(d.poverty))
         .attr("cy", d => yScale(d.healthcare))
-        .attr("r", "15")
+        .attr("r", "12")
         .attr("fill", "blue")
         .attr("opacity", ".5");
+
+    // Add Y Axis Title
+    chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height/2) - 10)
+    .attr("dy", "1em")
+    .attr("class", "aText")
+    .text("Lacks Healthcare (%)");
+
+    // Add Y Axis Title
+     chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.bottom-10})`)
+    .attr("class", "aText")
+    .text("In Poverty (%)");
+
+    // Add Chart Title
+    chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${-10})`)
+    .attr("class", "aText")
+    .text("Poverty vs. Healthcare");
+
 
 
 }).catch(error => console.log(error))
